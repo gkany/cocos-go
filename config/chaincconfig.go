899ed34e40acf3c7973b8920f80b0a5578538163
xukang <xukang@chukong-inc.com>
2019-12-12 18:01:45 +0800

    add chain

--- config/chaincconfig.go
+++ config/chaincconfig.go
@@ -19,6 +19,9 @@ const (
 	ChainIDObelisk = "1cfde7c388b9e8ac06462d68aadbd966b58f88797637d9af805b4560b0e9661e"
 	ChainIDGPH     = "b8d1603965b3eb1acba27e62ff59f74efa3154d43a4188d381088ac7cdf35539"
 	ChainIDKarma   = "c85b4a30545e09c01aaa7943be89e9785481c1e7bd5ee7d176cb2b3d8dd71a70"
+	ChainIDBCX     = "6057d856c398875cac2650fe33caef3d5f6b403d184c5154abbff526ec1143c4"
+	ChainIDBCXTest = "1ae3653a3105800f5722c5bda2b55530d0e9e8654314e2f3dc6d2b010da641c5"
+	ChainIDBCXDev  = "179db3c6a2e08d610f718f05e9cc2aabad62aff80305b9621b162b8b6f2fd79f"
 )
 
 var (
@@ -64,6 +67,24 @@ var (
 			CoreAsset: "KRM",
 			Prefix:    "KRM",
 			ID:        ChainIDKarma,
+		},
+		ChainConfig{
+			Name:      "cocosBCX",
+			CoreAsset: "COCOS",
+			Prefix:    "COCOS",
+			ID:        ChainIDBCX,
+		},
+		ChainConfig{
+			Name:      "cocosBCXTest",
+			CoreAsset: "COCOS",
+			Prefix:    "COCOS",
+			ID:        ChainIDBCXTest,
+		},
+		ChainConfig{
+			Name:      "cocosBCXDev",
+			CoreAsset: "COCOS",
+			Prefix:    "COCOS",
+			ID:        ChainIDBCXDev,
 		},
 	}
 )
